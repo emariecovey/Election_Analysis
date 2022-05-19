f82589b8cc0d2e2fc1f3e6b5a4ea3e690843b944
Emarie Swenson <emarieswenson@Emaries-MacBook-Air.local>
2022-05-18 20:51:40 -0600

    added election analysis on counties

--- Analysis/test.py
+++ Analysis/test.py
@@ -3,8 +3,20 @@
 voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
                 {"county":"Denver", "registered_voters": 463353},
                 {"county":"Jefferson", "registered_voters": 432438}]
+
+#data is a list of dictionaries
 print(type(voting_data))
-#print all dictionary
+
+#If there are 463353 registered voters, the county is denver
 for i in voting_data:
     if i["registered_voters"] == 463353:
         print(i["county"])
+
+#Print county name and number of registered voters
+for key in voting_data:
+    print(f"{key['county']} county has {key['registered_voters']} registered voters.")
+
+
+
+
+
